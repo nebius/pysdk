6b801882d1207a5f294f29da5511f0baeec6086a
Daniel D <d.drizhuk@gmail.com>
2026-08-24 13:38:04 +0200

    PUBLICAPI-1236: preparations to switch

--- nebius_generator/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Build-time generator for direct Nebius protobuf classes."""
--- nebius_generator/errors.py
+++
@@ -1,5 +0,0 @@
-"""User-facing generator failures."""
-
-
-class GeneratorError(ValueError):
-    """A deterministic user-facing generator failure."""
